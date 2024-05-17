97c431bbde07bd3d10294591fb5f6e279e9ad623
Alejandro Rondón <alejandro.rondon@bonitasoft.com>
2024-05-17 10:53:23 +0200

    First Commit

---
+++ src/test/java/com/bonitasoft/presales/connector/KafkaConsumerTest.java
@@ -0,0 +1,59 @@
+package com.bonitasoft.presales.connector;
+
+import org.bonitasoft.engine.connector.ConnectorException;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class KafkaConsumerTest {
+
+    KafkaConsumer connector;
+
+    @BeforeEach
+    void setUp() {
+        connector = new KafkaConsumer();
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_is_missing() {
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_is_empty() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(KafkaConsumer.DEFAULT_INPUT, "");
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_throw_exception_if_mandatory_input_is_not_a_string() {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(KafkaConsumer.DEFAULT_INPUT, 38);
+        connector.setInputParameters(parameters);
+        assertThrows(ConnectorValidationException.class, () ->
+                connector.validateInputParameters()
+        );
+    }
+
+    @Test
+    void should_create_output_for_valid_input() throws ConnectorException {
+        Map<String, Object> parameters = new HashMap<>();
+        parameters.put(KafkaConsumer.DEFAULT_INPUT, "valid");
+        connector.setInputParameters(parameters);
+        Map<String, Object> outputs = connector.execute();
+        assertThat(outputs).containsEntry("defaultOutput", "valid - output");
+    }
+
+}
